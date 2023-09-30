f2ec9a339277ee4b1be677766ae99cb930305151
SaUPL <96293439+sakshiupl@users.noreply.github.com>
2023-09-30 17:11:03 -0400

    Problem 4 Changes to dynamically switch between components

---
+++ p4.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import States from './components/states/States';
+import Example from './components/example/Example';
+
+class P4 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showExampleView: true,
+    }
+    this.handleView = this.handleView.bind(this);
+  }
+
+  handleView() {
+   this.setState({showExampleView: !this.state.showExampleView});
+  }
+
+  render() {
+    var {showExampleView} = this.state;
+    return (
+      <div>
+        <button onClick={this.handleView}>Switch to {showExampleView ? 'States' : 'Example'}</button>
+        {showExampleView ? <Example /> : <States />}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<P4 />, document.getElementById('reactapp'));
