e73545a6a2c97bd5a31c20fbb582d7ff195084a8
Sravani1915 <64299904+Sravani1915@users.noreply.github.com>
2023-10-01 20:26:14 -0400

    Pushing code for Problem 2

--- components/states/States.jsx
+++ components/states/States.jsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './States.css';
 
-/**
- * Define States, a React component of Project 4, Problem 2. The model
- * data for this view (the state names) is available at
- * window.models.states.
- */
-class States extends React.Component {
+
+class States extends Component {
   constructor(props) {
     super(props);
-    console.log('window.models.states', window.models.states);
+    this.state = {
+      substring: '', // State to store the input substring
+    };
   }
 
+  handleInputChange = (event) => {
+    // Update the substring state when the input value changes
+    this.setState({ substring: event.target.value });
+  };
+
   render() {
+    // Call the models.states() function to get the array of state names
+    const stateNames = models.states();
+
+    // Filter the state names based on the current substring
+    const filteredStates = stateNames.filter((state) =>
+      state.toLowerCase().includes(this.state.substring.toLowerCase())
+    );
+
     return (
       <div>
-        Replace this with the code for Project 4, Problem 2
+        <h2>States</h2>
+        <input
+          type="text"
+          placeholder="Enter substring"
+          value={this.state.substring}
+          onChange={this.handleInputChange}
+        />
+        <p>Filtering by Substring: {this.state.substring}</p>
+        <ul>
+          {filteredStates.length > 0 ? (
+            filteredStates.map((state, index) => (
+              <li key={index}>{state}</li>
+            ))
+          ) : (
+            <p>No matching states found.</p>
+          )}
+        </ul>
       </div>
     );
   }
